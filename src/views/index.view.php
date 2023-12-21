bafe9703807e4fe9da0467dc20cb355ef9a8b81b
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-22 01:44:52 +0630

    init folder structrue

--- src/views/index.view.php
+++ src/views/index.view.php
@@ -7,9 +7,9 @@
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- bootstrap css -->
-    <link rel="stylesheet" href="./bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
     <!-- bootstrap JS -->
-    <script src="./bootstrap.bundle.js"></script>
+    <script src="/js/bootstrap.bundle.js"></script>
     <title>OMS</title>
 </head>
 <body>
