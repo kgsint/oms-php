d342542fa8d8b84f8d151856e723076caeb34725
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-23 02:51:36 +0630

    rename MySQL.php to Database.php

--- src/Core/Database/Database.php
+++ src/Core/Database/Database.php
@@ -5,8 +5,9 @@ namespace App\Core\Database;
 use PDO;
 use PDOException;
 
-class MySQL 
+class Database 
 {
+    private string $connection;
     private string $host;
     private string $name;
     private string $user;
@@ -14,17 +15,19 @@ class MySQL
     private ?PDO $db = null;
 
     public function __construct(){
-          $this->host = $_ENV['DB_HOST'];
-          $this->name = $_ENV['DB_NAME'];
-          $this->user = $_ENV['DB_USER'];
-          $this->password = $_ENV['DB_PASSWORD'];
+        $this->connection = $_ENV['DB_CONNTECTION'];
+        $this->host = $_ENV['DB_HOST'];
+        $this->name = $_ENV['DB_NAME'];
+        $this->user = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASSWORD'];
     }
 
     public function connect()
     {
         try {
-            $dsn = "mysql:host={$this->host};dbname={$this->name}";
-
+            // data source name
+            $dsn = "{$this->connection}:host={$this->host};dbname={$this->name}";
+            // PDO instance
             $this->db = new PDO($dsn, $this->user, $this->password, [
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
