669cbbf3041bbdefac381c322608dc72968686a9
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-22 15:49:13 +0630

    refractor to expections api in Unit/RouterTest.php

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -10,38 +10,48 @@ it('registers get method', function() {
     $router = new Router;
     $router->get('/', fn() => "GET route");
 
-    test()->assertEquals('GET', $router->getRoutes()[0]['method']);
-    test()->assertEquals('/', $router->getRoutes()[0]['uri']);
+    expect($router->getRoutes()[0])->toMatchArray([
+        'method' => 'GET',
+        'uri' => '/',
+    ]);
 });
 
 it('it registers post method', function() {
     $router = new Router;
     $router->post('/store', fn() => "POST route");
 
-    test()->assertEquals('POST', $router->getRoutes()[0]['method']);
-    test()->assertEquals('/store', $router->getRoutes()[0]['uri']);
+    expect($router->getRoutes()[0])->toMatchArray([
+        'method' => 'POST',
+        'uri' => '/store',
+    ]);
 });
 
 it('registers put method', function() {
     $router = new Router;
     $router->put('/update', fn() => "PUT route");
 
-    test()->assertEquals('PUT', $router->getRoutes()[0]['method']);
-    test()->assertEquals('/update', $router->getRoutes()[0]['uri']);
+    expect($router->getRoutes()[0])->toMatchArray([
+        'method' => 'PUT',
+        'uri' => '/update',
+    ]);
 });
 
 it('it registers patch method', function() {
     $router = new Router;
     $router->patch('/update', fn() => "PATCH route");
 
-    test()->assertEquals('PATCH', $router->getRoutes()[0]['method']);
-    test()->assertEquals('/update', $router->getRoutes()[0]['uri']);
+    expect($router->getRoutes()[0])->toMatchArray([
+        'method' => 'PATCH',
+        'uri' => '/update',
+    ]);
 });
 
 it('it registers delete method', function() {
     $router = new Router;
     $router->delete('/delete', fn() => "DELETE route");
 
-    test()->assertEquals('DELETE', $router->getRoutes()[0]['method']);
-    test()->assertEquals('/delete', $router->getRoutes()[0]['uri']);
+    expect($router->getRoutes()[0])->toMatchArray([
+        'method' => 'DELETE',
+        'uri' => '/delete',
+    ]);
 });
