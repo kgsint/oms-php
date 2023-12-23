813ae86d24edff1aa53a23575ea7265bffb36a84
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-23 22:22:58 +0630

    refractor Router and RouterTest

--- src/Core/Http/Router.php
+++ src/Core/Http/Router.php
@@ -72,7 +72,7 @@ class Router
             }
         }
 
-        throw new RouteNotFoundException('Cannot find route end point for "' . $_SERVER['REQUEST_URI'] . '"');
+        throw new RouteNotFoundException('Cannot find route end point for "' . $uri . '"');
     }
 
     public function getRoutes(): array
