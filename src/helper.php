692676a21e8879310162511086c6efa46aa79339
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-24 03:22:49 +0630

    update helper file and add redirect function

--- src/helper.php
+++ src/helper.php
@@ -1,9 +1,13 @@
 <?php 
 
-function dd(mixed $value)
+function dd(mixed $value, $typeHint = false): void
 {
-    echo "<pre style='background-color: #111; color: white; padding:1em;'>";
-    var_dump($value);
+    echo "<pre style='background-color: #111; color: white; padding:1em; line-height: 1.8;'>";
+    if($typeHint) {
+        var_dump($value);
+    }else {
+        print_r($value);
+    }
     echo "</pre>";
     die;
 }
@@ -13,6 +17,12 @@ function mysqlTimestampToDateTime(string $timestamp): DateTime
     return new DateTime($timestamp);
 }
 
-function isActiveNav(string $uri) {
+function isActiveNav(string $uri): string
+{
     return parse_url($_SERVER['REQUEST_URI'])['path'] === $uri ? 'active' : '';
+}
+
+function redirect(string $uri, int $responseCode = 302): void
+{
+    header("Location:{$uri}", response_code: $responseCode);
 }
