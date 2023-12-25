da3410e062b852dd4b05e0b616bdff63dbbb20c1
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2023-12-25 13:02:47 +0630

    create Auth class and display authenticated user info

---
+++ src/Core/Auth.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Core;
+
+use App\Core\Database\Database;
+use App\Models\User;
+use App\Repositories\UserRepository;
+
+class Auth 
+{
+    private UserRepository $userRepo;
+
+    public function __construct()
+    {
+        $this->userRepo = new UserRepository(new Database);
+    }
+
+    // check authenticated or not
+    public static function check(): bool
+    {
+        if(empty($_SESSION['user'])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // get currently authenticated user model
+    public function user(): ?User
+    {
+        if(! Auth::check()) {
+            return null;
+        }
+
+        return $this->userRepo->find($_SESSION['user']['id']);
+    }
+}
--- src/views/_partials/navbar.php
+++ src/views/_partials/navbar.php
@@ -2,13 +2,19 @@
     <div class="flex-fill"></div>
 
     <navbar class="nav">
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown dropdown-center">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle = "dropdown">
                 <i class="fas fa-user-circle"></i>
             </a>
             <ul class="dropdown-menu">
                 <li>
-                    <a href="#" class="dropdown-item">Profile</a>
+                    <span class="dropdown-item">
+                        Profile 
+                        <br />
+                        <span class="text-muted"><?= (new \App\Core\Auth)->user()->name ?></span>
+                        <br />
+                        <span class="text-muted"><?= (new \App\Core\Auth)->user()->email ?></span>
+                    </span>
                 </li>
                 <li>
                     <button form="logout-form" class="dropdown-item">Logout</button>
